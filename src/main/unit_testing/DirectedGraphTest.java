b93d39bfce6d871d2e3f2d2dd886d858816fa5f5
Jeff Vazquez <jeffvazquez@cpp.edu>
2022-12-05 22:27:59 -0800

    Adding the unit tests for directed graph

--- src/main/unit_testing/DirectedGraphTest.java
+++ src/main/unit_testing/DirectedGraphTest.java
@@ -19,9 +19,61 @@ class DirectedGraphTest {
 	 * 		getTopologicalOrder()
 	 */
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	DirectedGraph<Character> testGraph = new DirectedGraph<>();
+
+    public static void createGraph(DirectedGraph<Character> graph)
+    {
+		directedCharGraph.addVertex('a');
+		directedCharGraph.addVertex('b');
+		directedCharGraph.addVertex('c');
+		directedCharGraph.addVertex('d');
+		directedCharGraph.addVertex('e');
+		directedCharGraph.addVertex('f');
+		directedCharGraph.addVertex('g');
+		directedCharGraph.addVertex('h');
+		directedCharGraph.addVertex('i');
+		
+		// connect the edges
+		directedCharGraph.addEdge('a', 'b');
+		directedCharGraph.addEdge('a', 'd');
+		directedCharGraph.addEdge('a', 'e');
+		directedCharGraph.addEdge('b', 'e');
+		directedCharGraph.addEdge('d', 'g');
+		directedCharGraph.addEdge('e', 'f');
+		directedCharGraph.addEdge('e', 'h');
+		directedCharGraph.addEdge('g', 'h');
+		directedCharGraph.addEdge('f', 'c');
+		directedCharGraph.addEdge('f', 'h');
+		directedCharGraph.addEdge('h', 'i');
+		directedCharGraph.addEdge('c', 'b');
+		directedCharGraph.addEdge('i', 'f');
 	}
+	@Test
+    public void testgetNumberOfNodes()
+    {
+        createGraph(testGraph);
+        assertEquals(13, testGraph.getNumberOfEdges());
+    }
+    @Test
+    public void testIsEmpty()
+    {
+        assertEquals(true, testGraph.isEmpty());
+        createGraph(testGraph);
+        assertEquals(false, testGraph.isEmpty());
+    }
+
+
+    @Test
+    public void testHasEdge()
+    {
+        createGraph(testGraph);
+        assertEquals(true, testGraph.hasEdge("A","B"));
+        assertEquals(true, testGraph.hasEdge("B","E"));
+        assertEquals(true, testGraph.hasEdge("G","H"));
+        assertEquals(true, testGraph.hasEdge("F","C"));
+        assertEquals(true, testGraph.hasEdge("I","F"));
+    }
+}
+
 
 }
